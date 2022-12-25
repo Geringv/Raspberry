a35f9a10a8e5fe5036e5d47115577ca441811f55
Geringv <103842593+Geringv@users.noreply.github.com>
2022-12-25 13:09:02 +0100

    optimization
    optimized code

--- water.py
+++ water.py
@@ -1,44 +1,37 @@
+#import dependencies
 import RPi.GPIO as GPIO
 from time import sleep
 
-sensor1 = 17
-sensor2 = 27
-pump1 = 5
-pump2 = 6
-count1 = 0
-count2 = 0
+#variables
+sensoren = [17,27]
+pumpen = [5,6]
+count1,count2 = 0
 
+#numbering
 GPIO.setmode(GPIO.BCM)
+#pin setup
+GPIO.setup(sensoren,GPIO.IN)
+GPIO.setup(pumpen,GPIO.OUT,initial = 1)
 
-GPIO.setup(sensor1,GPIO.IN)
-GPIO.setup(sensor2,GPIO.IN)
-GPIO.setup(pump1,GPIO.OUT,initial = 1)
-GPIO.setup(pump2,GPIO.OUT,initial = 1)
+#variable function for watering
+def watering(sensorID,pumpeID,pflanzeID,gießzeit):
+    if GPIO.input(sensoren(sensorID)) == 1: #check moisture sensor for dryness
+        print(f"plant {pflanzeID} dry")
+        GPIO.output(pumpen(pumpeID),0) #turn pump on
+        sleep(gießzeit) #water plant for x seconds
+        GPIO.output(pumpen(pumpeID),1) #turn pump off
+        count1 += 1
+        print(f"plant {pflanzeID} watered " + str(count1) + " times")
+    else:
+        print(f"plant {pflanzeID} wet")
+
+#executing
 try:
     while True:
-        if GPIO.input(sensor1) == 1:
-            print("plant 1 dry")
-            GPIO.output(pump1,0)
-            sleep(3)
-            GPIO.output(pump1,1)
-            count1 += 1
-            print("plant 1 watered " + str(count1) + " times")
-            
+        watering(0,0,1,3) #plant 1 checked / watered
+        watering(1,1,2,3) #plant 2 checked / watered
+        sleep(1) #sleep for 1 second to save cpu
 
-        elif GPIO.input(sensor2) == 1:
-            print("plant 2 dry")
-            GPIO.output(pump2,0)
-            sleep(3)
-            GPIO.output(pump2,1)
-            count2 += 1
-            print("plant 1 watered " + str(count2) + " times")
-            
-
-        else:
-            print("both plants wet")
-        sleep(1)
-
-except KeyboardInterrupt:
-    GPIO.output(pump1,False)
-    GPIO.output(pump2,False)
-    GPIO.cleanup()
+except KeyboardInterrupt: #quit if strg + c is pressed
+    GPIO.output(pumpen,False) # set pumps to off
+    GPIO.cleanup() #free cpu
